b5f927b78c3ef7592516b1963dc4e2e92880e9ab
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-12-12 13:26:01 -0500

    Day 1 Part 2

---
+++ Day1/Part2/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::pair<char, int> ParseLine(const std::string& currentLine);
+
+int main() {
+  // Declare variables to hold values
+  int currentNum{50};
+  int zeroCounter{0};
+  constexpr int minValue{0};
+  constexpr int maxValue{99};
+  std::string currentLine;
+
+  // Open the input file
+  std::ifstream input("input.txt");
+  if(!input) {
+    std::cerr << "Could not open input txt file.\n";
+    return 1;
+  }
+  
+  //Process each line in the file
+  while(std::getline(input, currentLine)) { // Increments to next line when complete
+    auto [nextDirection, numClicks] = ParseLine(currentLine);
+    
+    if(nextDirection == 'L') {  // Left decreases number
+      // Iterate over each click
+      for(int i = 0; i < numClicks; i++) {
+        currentNum--;
+        if(currentNum < minValue) {
+          currentNum = maxValue;
+        }
+        if(currentNum == 0) {
+          zeroCounter++;
+        }
+      }
+//      currentNum -= numClicks;
+//      while(currentNum < minValue) {
+//        currentNum = ((maxValue + 1) - abs(currentNum));
+//      }
+    }
+
+    else if(nextDirection == 'R') {  // Right increases number
+      // Iterate over each click
+      for(int i = 0; i < numClicks; i++) {
+        currentNum++;
+        if(currentNum > maxValue) {
+          currentNum = minValue;
+        }
+        if(currentNum == 0) {
+          zeroCounter++;
+        }
+      }
+//      currentNum += numClicks;
+//      while(currentNum > maxValue) {
+//        currentNum = ((currentNum - 1) - maxValue);
+//      }
+    }
+
+//    if(currentNum == 0) {
+//      zeroCounter++;
+//    }
+  }
+
+  std::cout << zeroCounter;
+
+  input.close();
+  return 0;
+}
+
+std::pair<char, int> ParseLine(const std::string& currentLine) {
+  char letter = currentLine[0];
+  int number = std::stoi(currentLine.substr(1));
+  return{letter, number};
+}
