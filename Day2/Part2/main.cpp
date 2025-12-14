0eea144c246eb24110529b2780f693806ee1d8a0
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-12-14 09:46:59 -0500

    Factor out unnecessary vector

--- Day2/Part2/main.cpp
+++ Day2/Part2/main.cpp
@@ -2,13 +2,12 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <vector>
 #include <regex>
 
 int main() {
   std::string currentRange;
-  std::vector<std::pair<long long, long long>> ranges;
   long long invalidSum{0};
+  std::regex repeatedNum(R"(^(\d+)\1+$)");
 
   std::ifstream input("input.txt");
   if (!input) {
@@ -21,16 +20,8 @@ int main() {
     char dash;
     std::stringstream range(currentRange);
     range >> start >> dash >> end;
-    ranges.emplace_back(start, end);
-  }
-
-  // Use regex to calculate the repeated half
-  std::regex repeatedNum(R"(^(\d+)\1+$)");
-
-  for (auto& [start, end] : ranges) {
     for (long long i = start; i <= end; i++) {
       std::string s = std::to_string(i);
-
       if (std::regex_match(s, repeatedNum)) {
         invalidSum += i;
       }
