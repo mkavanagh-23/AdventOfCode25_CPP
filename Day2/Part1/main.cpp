928ec29aca4441f897288e8ca57c2de28de954f6
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-12-14 09:00:54 -0500

    Structure for parts

--- Day2/Part1/main.cpp
+++ Day2/Part1/main.cpp
@@ -1,5 +1,3 @@
-//  For each range iterate ovber each value within. If mod by 2 is equal to zero it can possibly be invalid. Convert to string, split to two substrings, check if substrings are equal. If invalid, convert back to an int and add to the invalid sum.
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -23,7 +21,7 @@ int main() {
     long long start, end;
     char dash;
     std::stringstream range(currentRange);
-    range >> start >> dash >> end;  // Read the values into the variables
+    range >> start >> dash >> end;
     ranges.emplace_back(start, end);
   }
 
@@ -32,7 +30,6 @@ int main() {
     for(long long i = start; i <= end; i++) {
       std::string valString = std::to_string(i);
       int length = valString.length();
-      // Mod length by 2 to see if we have even num of digits
       if(length % 2 == 0) {
         std::string first = valString.substr(0, length/2);
         std::string second = valString.substr(length/2, length/2);
